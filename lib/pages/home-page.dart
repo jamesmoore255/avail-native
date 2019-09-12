61d895c9fc11ab6a153c8c6114a30f023837e727
jamesmoore255 <email_id>
2019-09-11 21:26:10 -0500

    Authentication implemented

--- lib/pages/home-page.dart
+++ lib/pages/home-page.dart
@@ -1,19 +1,69 @@
+import 'package:avail/pages/auth-page.dart';
 import "package:avail/shared/filter-chip-bar.dart";
+import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 import "package:cloud_firestore/cloud_firestore.dart";
 
 final databaseReference = Firestore.instance;
 
 class HomePage extends StatefulWidget {
+  HomePage(
+      {Key key,
+      @required this.signedIn,
+      @required this.signOut,
+      @required this.user,
+      @required this.userData})
+      : super(key: key);
+  final bool signedIn;
+  final GestureTapCallback signOut;
+  final FirebaseUser user;
+  final Map<String, dynamic> userData;
+
+  @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
+  bool signedIn = false;
+
+  homeContents() {
     return Scaffold(
       bottomNavigationBar: BottomAppBar(
-        child: HomeBottomAppBarContents(),
+        child: Container(
+          height: 48.0,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, "/auth");
+                  },
+                  child: CircleAvatar(backgroundColor: Color(0xFF484848))),
+              GestureDetector(
+                child: Icon(Icons.image, size: 40),
+                onTap: () {
+                  Navigator.pushNamed(context, "/depiction");
+                },
+              ),
+              VerticalDivider(
+                width: 32,
+              ),
+              GestureDetector(
+                child: Icon(Icons.note, size: 40),
+                onTap: () {
+                  Navigator.pushNamed(context, "/script");
+                },
+              ),
+              GestureDetector(
+                child: Icon(Icons.music_note, size: 40),
+                onTap: () {
+                  Navigator.pushNamed(context, "/sound");
+                },
+              ),
+            ],
+          ),
+        ),
         elevation: 8,
       ),
       floatingActionButton: FloatingActionButton(
@@ -22,109 +72,73 @@ class _HomePageState extends State<HomePage> {
         onPressed: null,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: HomeBody(),
-    );
-  }
-}
-
-class HomeBottomAppBarContents extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        height: 48.0,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, "/auth");
-                },
-                child: CircleAvatar(backgroundColor: Color(0xFF484848))
-            ),
-            GestureDetector(
-              child: Icon(Icons.image, size: 40),
-              onTap: () {
-                Navigator.pushNamed(context, "/depiction");
-              },
-            ),
-            VerticalDivider(
-              width: 32,
-            ),
-            GestureDetector(
-              child: Icon(Icons.note, size: 40),
-              onTap: () {
-                Navigator.pushNamed(context, "/script");
-              },
-            ),
-            GestureDetector(
-              child: Icon(Icons.music_note, size: 40),
-              onTap: () {
-                Navigator.pushNamed(context, "/sound");
-              },
-            )
-          ],
-        ));
-  }
-}
-
-class HomeBodyState extends State<HomeBody> {
-//  Widget _searchChipBar() {
-//    return IconButton(
-//      icon: Icon(Icons.filter_list),
-//      onPressed: null,
-//    );
-//  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      children: <Widget>[
-        SafeArea(
-          child: ChipBar(),
-        ),
-        Expanded(
-          child: StreamBuilder(
+      body: Column(
+        children: <Widget>[
+          SafeArea(
+            child: ChipBar(),
+          ),
+          Expanded(
+            child: StreamBuilder(
               stream: databaseReference.collection("test").snapshots(),
               builder: (context, snapshot) {
                 if (!snapshot.hasData) return const Text("Loading...");
                 return ListView.builder(
-                    padding: EdgeInsets.all(16),
-                    itemCount: snapshot.data.documents.length,
-                    itemBuilder: (BuildContext context, int i) {
-                      DocumentSnapshot testDoc = snapshot.data.documents[i];
-                      List<Widget> subsections = <Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Text(testDoc["title"], style: Theme.of(context).textTheme.subhead),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Text(testDoc["type"]),
-                        )
-                      ];
-                      return Card(
-                          elevation: 1,
-                          child: Row(
-                            children: <Widget>[
-                              Padding(
-                                  child: Text("Media"),
-                                  padding: EdgeInsets.all(8)),
-                              Column(
-                                children: subsections,
-                              )
-                            ],
-                          ));
-                    });
-              }),
-        )
-      ],
-    ));
+                  padding: EdgeInsets.all(16),
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (BuildContext context, int i) {
+                    DocumentSnapshot testDoc = snapshot.data.documents[i];
+                    List<Widget> subsections = <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Text(testDoc["title"],
+                            style: Theme.of(context).textTheme.subhead),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Text(testDoc["type"]),
+                      )
+                    ];
+                    return Card(
+                      elevation: 1,
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                              child: Text("Media"), padding: EdgeInsets.all(8)),
+                          Column(
+                            children: subsections,
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
-}
 
-class HomeBody extends StatefulWidget {
   @override
-  HomeBodyState createState() => HomeBodyState();
+  Widget build(BuildContext context) {
+    if (widget.signedIn != signedIn) {
+      setState(() {
+        signedIn = widget.signedIn;
+      });
+    }
+    return IndexedStack(
+      index: signedIn ? 0 : 1,
+      children: <Widget>[
+        Container(
+          child: homeContents(),
+        ),
+        AnimatedOpacity(
+          child: AuthPage(),
+          opacity: signedIn ? 0 : 1,
+          duration: Duration(milliseconds: 500),
+        ),
+      ],
+    );
+  }
 }
